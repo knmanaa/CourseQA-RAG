80cb751d07a4d6aaef8ae4547a22e45af39091ce
pfp33780 <wukinghim26@gmail.com>
2026-03-21 02:01:56 +0800

    Prepared raw lecture notes as dataset. Preprocessed lecture notes to a ready json with identifiable id

--- dataset/doc_processing_util.py
+++ dataset/doc_processing_util.py
@@ -4,10 +4,23 @@ For individual courses, plz view the corresponding util.py
 """
 
 import json
+
+from pathlib import Path
+from typing import Dict
 import pymupdf as pdf
 
-def extract_text_outside_tables():
-    print("balabababa")
+def rename_notes(notes_path: str) -> None:
+    notes_path = Path(notes_path)
+
+    items = sorted(notes_path.iterdir(), key=lambda p: p.name.lower())
+
+    # rename each file in a dir to ch1, ch2, ...
+    for i, doc_path in enumerate(items, 1):
+        print(doc_path)
+        new_name = f"{doc_path.parent}/ch{i}.pdf"
+        doc_path.rename(new_name)
+        print(new_name)
+
 
 def extract_table(page: pdf.Page) -> list[list[list[str]]]:
 
@@ -26,35 +39,43 @@ def extract_table(page: pdf.Page) -> list[list[list[str]]]:
                     result[table][row][cell] = result[table][row][cell].replace("\n", " ")
 
         return result
+    
 
-testing = {
-    "Course 1 Name" : {
-        1 : {
-            "text" : ["Apple", "Banana", "Cucumber"],
-            "table": [["English", "German", "Vietnamese"], ["Hello", "Hallo", "Xin chao"]]
-        }, 
+def make_one_course_dict(notes_path: str, course_json_target_path = "/shared/project/CourseQA-RAG/dataset/datasets/courses") -> Dict:
 
-        2 : {
-            "text" : ["Dog", "Egg", "Flower"],
-            "table": [["English", "German", "Vietnamese"], ["1", "2", "3"]]
-        }
-    }, 
+    course_dict = {}
+    for doc_path in Path(notes_path).iterdir(): # iterate each chapter
+        doc = pdf.open(doc_path) # open one chapter
 
-    "Course 2 Name" : {
-        1 : {
-            "text" : ["Apple", "Banana", "Cucumber"],
-            "table": [["English", "German", "Vietnamese"], ["Bye", "bye bye", "BYE BYE BYE"]]
-        }, 
+        for page in range(doc.page_count): # iterate each page
 
-        2 : {
-            "text" : ["Dog", "Egg", "Flower"],
-            "table": [["English", "German", "Vietnamese"], ["10", "20", "30"]]
-        }
-    }
-}
+            page_name = str(page + 1)
+            chapter_name = Path(doc_path).stem
+            course_name = Path(notes_path).name
 
-with open("/shared/project/CourseQA-RAG/dataset/testing.json", "w", encoding = "utf-8") as f:
-    json.dump(testing, f, indent = 4)
+            course_dict_key = course_name + "_" + chapter_name + "_" + page_name
+            course_dict_value = doc[page].get_text("text")
 
+            course_dict[course_dict_key] = course_dict_value
 
+    with open(Path(course_json_target_path) / str(course_name + ".json"), "w") as f:
+        json.dump(course_dict, f, indent = 4)
 
+    return course_dict
+
+def make_many_course_dict(courses_path = "/shared/project/CourseQA-RAG/data/raw/lectures", course_json_target_path = "/shared/project/CourseQA-RAG/dataset/datasets/courses"):
+    courses_path = Path(courses_path)
+
+    result = {}
+
+    for notes_path in courses_path.iterdir():
+
+        if not Path.is_dir(notes_path): continue
+
+        course_dict = make_one_course_dict(notes_path, course_json_target_path)
+        result |= course_dict
+
+    with open(Path(course_json_target_path) / "all_courses.json", "w") as f:
+        json.dump(result, f, indent = 4)
+
+    return result
